92a539da297b115815760b40dd96dabe5272c3f7
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-27 20:00:36 +0530

    Add files via upload

---
+++ terratest-poc/variable.tf
@@ -0,0 +1,26 @@
+variable "location" {
+  type    = string
+}
+
+/* variable "postfix" {
+  type    = string
+}
+variable "prefix" {
+  type    = string
+} */
+
+variable "resource_group_name" {
+  type    = string
+}
+
+variable "resource_name" {
+  type    = string
+}
+
+variable "address_space" {
+  type  = list
+}
+
+variable "tags" {
+  type  = map
+}
